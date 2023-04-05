56eb72281c8b4f5b35e022bcc79aa655344633f7
leon <leonsk32@gmail.com>
2023-04-05 11:29:32 +0900

    communicate with OpenAI on Azure

--- src/handler.ts
+++ src/handler.ts
@@ -4,26 +4,108 @@ import {
   AwsEvent,
 } from "@slack/bolt/dist/receivers/AwsLambdaReceiver"
 import Lambda from "aws-lambda"
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai"
+
+const SYSTEM_MESSAGE_PREFIX = ":writing_hand:"
+const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET
+const SLACK_BOT_USER_OAUTH_TOKEN = process.env.SLACK_BOT_USER_OAUTH_TOKEN
+const SLACK_BOT_MEMBER_ID = process.env.SLACK_BOT_MEMBER_ID
+const OPENAI_RESOURCE_GROUP = process.env.OPENAI_RESOURCE_GROUP
+const OPENAI_MODEL_DEPLOY_NAME = process.env.OPENAI_MODEL_DEPLOY_NAME
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+const OPENAI_API_VERSION = process.env.OPENAI_API_VERSION
 
 const receiver = new AwsLambdaReceiver({
-  signingSecret: process.env.SLACK_SIGNING_SECRET ?? "",
+  signingSecret: SLACK_SIGNING_SECRET ?? "",
 })
 
 const app = new App({
-  token: process.env.SLACK_BOT_USER_OAUTH_TOKEN,
+  token: SLACK_BOT_USER_OAUTH_TOKEN,
   receiver,
 })
 
-app.event("app_mention", async ({ event, say }) => {
+const configuration = new Configuration({
+  basePath: `https://${OPENAI_RESOURCE_GROUP}.openai.azure.com/openai/deployments/${OPENAI_MODEL_DEPLOY_NAME}`,
+})
+const openAiApi = new OpenAIApi(configuration)
+
+app.event("app_mention", async ({ event, context, client, say }) => {
+  if (context.retryNum) {
+    console.log("skip retry")
+    return
+  }
+
+  console.log(event)
+  const { channel, thread_ts, event_ts } = event
+  // thread_ts is undefined when the event is not a thread
+  const timestamp = thread_ts ?? event_ts
+
+  await say({
+    channel,
+    thread_ts: timestamp,
+    text: `${SYSTEM_MESSAGE_PREFIX} I'm thinking...`,
+  })
+
+  const replies = await client.conversations.replies({
+    channel,
+    ts: timestamp,
+  })
+
+  const requestMessages: ChatCompletionRequestMessage[] =
+    replies.messages?.reduce(
+      (messages: ChatCompletionRequestMessage[], message) => {
+        const { text, user } = message
+        if (!text) return messages
+
+        if (user !== SLACK_BOT_MEMBER_ID) {
+          messages.push({
+            role: "user",
+            content: text.replace(`<@${SLACK_BOT_MEMBER_ID}>`, ""),
+          })
+          return messages
+        }
+
+        if (!text.startsWith(SYSTEM_MESSAGE_PREFIX)) {
+          messages.push({
+            role: "assistant",
+            content: text,
+          })
+          return messages
+        }
+
+        return messages
+      },
+      []
+    ) ?? []
+
   try {
-    const { channel, event_ts } = event
-    await say({
+    const completion = await openAiApi.createChatCompletion(
+      {
+        model: OPENAI_MODEL_DEPLOY_NAME ?? "",
+        messages: requestMessages,
+      },
+      {
+        headers: {
+          "api-key": OPENAI_API_KEY ?? "",
+        },
+        params: { "api-version": OPENAI_API_VERSION ?? "" },
+      }
+    )
+    const outputText = completion.data.choices
+      .map(({ message }) => message?.content ?? "")
+      .join("")
+    await client.chat.postMessage({
       channel,
-      thread_ts: event_ts,
-      text: "Hello World!",
+      thread_ts: timestamp,
+      text: outputText,
     })
-  } catch (error) {
-    console.error(error)
+  } catch (e) {
+    console.log(e)
+    await client.chat.postMessage({
+      channel,
+      thread_ts: timestamp,
+      text: `${SYSTEM_MESSAGE_PREFIX} something went wrong...\n${e}`,
+    })
   }
 })
 
